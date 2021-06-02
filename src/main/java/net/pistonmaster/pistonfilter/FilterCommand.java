33a2b7340f5e606c0b7198ea62b823037e20d7fe
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-02 22:19:35 +0200

    Use proper message system for command messages

--- src/main/java/net/pistonmaster/pistonfilter/FilterCommand.java
+++ src/main/java/net/pistonmaster/pistonfilter/FilterCommand.java
@@ -1,7 +1,6 @@
 package net.pistonmaster.pistonfilter;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,28 +19,24 @@ public class FilterCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender.hasPermission("pistonfilter.admin")) {
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("reload")) {
-                    plugin.loadConfig();
-                    sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GOLD + "Reloaded the config!"));
+        if (sender.hasPermission("pistonfilter.admin") && args.length > 0) {
+            if (args[0].equalsIgnoreCase("reload")) {
+                plugin.loadConfig();
+                sender.sendMessage(ChatColor.GOLD + "Reloaded the config!");
+            }
+
+            if (args[0].equalsIgnoreCase("add") && args.length > 1) {
+                FileConfiguration config = plugin.config;
+
+                config.set("banned-text", plugin.config.getStringList("banned-text").add(args[1]));
+
+                try {
+                    config.save(new File(plugin.getDataFolder(), "config.yml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
 
-                if (args[0].equalsIgnoreCase("add")) {
-                    if (args.length > 1) {
-                        FileConfiguration config = plugin.config;
-
-                        config.set("banned-text", plugin.config.getStringList("banned-text").add(args[1]));
-
-                        try {
-                            config.save(new File(plugin.getDataFolder(), "config.yml"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GOLD + "Successfully added the config entry!"));
-                    }
-                }
+                sender.sendMessage(ChatColor.GOLD + "Successfully added the config entry!");
             }
         }
 
